96ae6af65963288e0cbd430d845d22e76b49fc81
ShashankMishra03 <shashankpandit2018@gmail.com>
2024-01-03 18:50:10 +0530

    comment problem solved

--- Blog Web App/backend/server.js
+++ Blog Web App/backend/server.js
@@ -613,7 +613,7 @@ app.post("/get-replies", (req, res) => {
   Comment.findOne({ _id })
     .populate({
       path: "children",
-      option: {
+      options: {
         limit: maxLimit,
         skip: skip,
         sort: { commentedAt: -1 },
